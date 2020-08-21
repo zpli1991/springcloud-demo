5372860a3abafc00bf47709cc0f1be944bffc7c7
lizongping <438985408@qq.com>
2020-08-21 10:31:25 +0800

    初始化工程

---
+++ microservicecloud-api/src/main/java/org/zpli/ApiApplicationStart.java
@@ -0,0 +1,13 @@
+package org.zpli;
+
+/**
+ * Hello world!
+ *
+ */
+public class ApiApplicationStart
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ microservicecloud-api/src/test/java/org/zpli/ApiApplicationStartTest.java
@@ -0,0 +1,20 @@
+package org.zpli;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple ApiApplicationStart.
+ */
+public class ApiApplicationStartTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
